ad9b5822c08cf186a730d84fb22805c54b71913b
arjunah <arjunah@abv.bg>
2019-12-11 01:13:09 +0200

    styles

--- src/register/index.js
+++ src/register/index.js
@@ -1,78 +1,12 @@
 import React from "react";
 import CredentialsForm from "../credentials-form"
 
-function Register () {
+function Register() {
     return (
         <div className="Register">
             <h1>Registration Form</h1>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <p>Registered users get the best deals on our products. Register now to enjoy discounts, special offers and more!</p>
-            <CredentialsForm registration={true}/>
+            <p>Registered users get the best deals on our products. <br/>Register now to enjoy discounts, special offers and more!</p>
+            <CredentialsForm registration={true} />
         </div>
     )
 }
